22f63a2ff1b1ba809503e11f1fb59bf116b8739d
Gaetano26 <gaetano.spampinato@outlook.it>
2023-04-19 21:05:32 +0200

    fixed cols FilmsComponent

--- src/components/FilmsComponent.vue
+++ src/components/FilmsComponent.vue
@@ -3,13 +3,13 @@
         <div class="row pt-5">
             <h3 class="text-white text-center">Films</h3>
         </div>
-         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 gap-1s justify-content-center">
+         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 gap-1s justify-content-center">
             <CardComponents/>
         </div>
         <div class="row mt-5">
             <h3 class="text-white text-center">Serie tv</h3>
         </div>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 gap 1 justify-content-center pb-4">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 gap 1 justify-content-center pb-4">
             <ListComponent/>
         </div>    
     </div>
