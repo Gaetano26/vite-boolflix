d8c8a811bc3c4b8f8d32736dd453540a1d550c77
Gaetano26 <gaetano.spampinato@outlook.it>
2023-04-20 16:30:05 +0200

    testing

--- src/components/FilmsComponent.vue
+++ src/components/FilmsComponent.vue
@@ -3,13 +3,13 @@
         <div class="row pt-5">
             <h3 class="text-white text-center">Films</h3>
         </div>
-         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 gap-1s justify-content-center altz">
+         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5  justify-content-center altz">
             <CardComponents/>
         </div>
         <div class="row mt-5">
             <h3 class="text-white text-center mt-5">Serie tv</h3>
         </div>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 gap 1 justify-content-center pb-5 altz">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5  justify-content-center pb-5 altz">
             <ListComponent/>
         </div>    
     </div>
